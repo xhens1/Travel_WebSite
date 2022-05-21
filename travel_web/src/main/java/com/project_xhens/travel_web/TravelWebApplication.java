f645219a055924e9f248e1e0f49835bd27a3c54f
정아윤 <ptotmt5421@nate.com>
2022-05-21 18:26:39 +0900

    init setting

---
+++ travel_web/src/main/java/com/project_xhens/travel_web/TravelWebApplication.java
@@ -0,0 +1,13 @@
+package com.project_xhens.travel_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelWebApplication.class, args);
+	}
+
+}
---
+++ travel_web/src/test/java/com/project_xhens/travel_web/TravelWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_xhens.travel_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
